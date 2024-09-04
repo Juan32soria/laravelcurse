cb042f579fcbaaa588da9d5f60a246faa7b0998d
Juan32soria <91746093+Juan32soria@users.noreply.github.com>
2024-09-03 19:41:00 -0500

    Taller 2

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,5 @@
 <?php
-
+ 
 use Illuminate\Support\Facades\Route;
 Route::get('/', 'App\Http\Controllers\HomeController@index')->name("home.index");
 Route::get('/about', function () {
@@ -12,7 +12,7 @@ Route::get('/about', function () {
       ->with("description", $description)
       ->with("author", $author);
 })->name("home.about");
-Route::get('/products', 'App\Http\Controllers\ProductController@index')->name("product.index");
+Route::get('/products', 'App\Http\Controllers\ProductController@index')->name("product.index"); //crear con base a este llamado de ruta
 Route::get('/products/create', 'App\Http\Controllers\ProductController@create')->name("product.create");
 Route::post('/products/save', 'App\Http\Controllers\ProductController@save')->name("product.save");
 Route::get('/products/{id}', 'App\Http\Controllers\ProductController@show')->name("product.show");
